b9b722cac805c54adbfd34acf3659d883d3d8663
khj1 <dhepd8849@gmail.com>
2022-11-28 15:32:47 +0900

    feat: 로또 번호를 입력 받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,11 +1,30 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.Lotto;
 import lotto.Money;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputView {
 
+    public static final String REGEX = ",";
+
     public Money readMoney() {
         return Money.from(Console.readLine());
+    }
+
+    public Lotto readLottoNumbers() {
+        String[] separatedByRegex = Console.readLine().split(REGEX);
+
+        return Lotto.from(getNumbers(separatedByRegex));
+    }
+
+    private static List<Integer> getNumbers(String[] separatedNumbers) {
+        return Arrays.stream(separatedNumbers)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
