22b3e20635863b8af6134382ea6bad600b370fcf
khj1 <dhepd8849@gmail.com>
2022-11-28 15:46:11 +0900

    feat: 기본적인 입력 안내문 출력 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.GuidanceMessage;
 import lotto.Lotto;
 import lotto.LottoNumber;
 import lotto.Money;
@@ -14,13 +15,25 @@ public class InputView {
     public static final String REGEX = ",";
 
     public Money readMoney() {
+        printRequestMoneyMessage();
+
         return Money.from(Console.readLine());
     }
 
+    private void printRequestMoneyMessage() {
+        System.out.println(GuidanceMessage.REQUEST_MONEY);
+    }
+
     public Lotto readLottoNumbers() {
+        printRequestWinningNumbers();
+
         String[] separatedByRegex = Console.readLine().split(REGEX);
 
         return Lotto.from(getNumbers(separatedByRegex));
+    }
+
+    private void printRequestWinningNumbers() {
+        System.out.println(GuidanceMessage.REQUEST_WINNING_NUMBERS);
     }
 
     private static List<Integer> getNumbers(String[] separatedNumbers) {
@@ -30,6 +43,12 @@ public class InputView {
     }
 
     public LottoNumber readBonusNumber() {
+        printRequestBonusNumber();
+
         return LottoNumber.from(Console.readLine());
+    }
+
+    private void printRequestBonusNumber() {
+        System.out.println(GuidanceMessage.REQUEST_BONUS_NUMBER);
     }
 }
